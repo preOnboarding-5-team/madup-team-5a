25891eb0b1d1a9bc9483cc191fad12eb2b970914
restareaByWeezy <64529155+restareaByWeezy@users.noreply.github.com>
2022-05-24 14:01:38 +0900

    chart axis needs to be fixed

--- src/components/AdStatus/StatusChart.tsx
+++ src/components/AdStatus/StatusChart.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { VictoryChart, VictoryLine, VictoryTheme, VictoryTooltip, VictoryVoronoiContainer } from 'victory';
+import { VictoryAxis, VictoryChart, VictoryLine, VictoryTheme, VictoryTooltip, VictoryVoronoiContainer } from 'victory';
 import dayjs from 'dayjs';
 
 import TRAND_DATA from '../../data/wanted_FE_trend-data-set.json';
@@ -9,7 +9,7 @@ import styles from './StatusChart.module.scss';
 import { ScalePropType } from 'victory-core';
 import { Daily } from 'types/trend';
 import { convertData } from 'utils/convertStatusData';
-import type { TableKey } from 'utils/convertStatusData';
+import type { TableKey, Data } from 'utils/convertStatusData';
 
 // ROAS, 광고비, 노출수, 클릭수, 전환 수, 매출 중 선택가능.
 // 첫 드롭다운에서 선택한 지표를 두번째 드롭다운에서 선택할 수 없으며, 두번재 드롭다운 선택은 옵셔널임
@@ -17,42 +17,55 @@ import type { TableKey } from 'utils/convertStatusData';
 
 const options = {
   width: 960,
-  height: 274,
+  height: 300,
   padding: {
-    left: 0,
-    top: 0,
+    left: 60,
+    top: 10,
     right: 60,
     bottom: 30,
   },
   scale: { x: 'time' as ScalePropType },
 };
 
+// const categories: TableKey[] = ['click' ,'imp' ,'cost' ,'conv' , 'ctr']
+
 const StatusChart = () => {
   const table = convertData(TRAND_DATA.report.daily as Daily[]);
 
   const [dates, setDates] = useState(['2022-02-01', '2022-02-02', '2022-02-03', '2022-02-04']);
   const [category, setCategory] = useState<TableKey>('cost');
+  const [subCategory, setSubCategory] = useState<TableKey>('conv');
+  // const [category, setCategory] = useState(0);
+  // const [subCategory, setSubCategory] = useState(0);
   const [dayOrWeek, setDayOrWeek] = useState(true);
 
   // 1주일치
   const weekly = [];
-  let weeklyData: ({ x: string; y: number } | undefined)[] = [];
+  let weeklyMainData: Data[] = [];
+  let weeklySubData: Data[] = [];
 
   for (let i = 0; i < 7; i++) {
     weekly.push(dayjs(dates[0]).add(i, 'd').format('YYYY-MM-DD'));
   }
-
+  console.log(weekly);
   weekly.forEach((date) => {
-    weeklyData.push(table[category].find((data) => data.x === date));
+    weeklyMainData.push(table[category].find((data) => data.x === date) as Data);
+    weeklySubData.push(table[subCategory].find((data) => data.x === date) as Data);
   });
-  console.log(weeklyData);
+
+  // setWeeklyMData(weeklyMainData)
 
   // 일별
-  let dailyData: ({ x: string; y: number } | undefined)[] = [];
-
+  let dailyMainData: Data[] = [];
+  let dailySubData: Data[] = [];
   dates.forEach((date) => {
-    dailyData.push(table[category].find((data) => data.x === date));
+    dailyMainData.push(table[category].find((data) => data.x === date) as Data);
+    dailySubData.push(table[subCategory].find((data) => data.x === date) as Data);
   });
+
+  const getMax = (arr: { x: string; y: number }[]) => {
+    return Math.max(...arr.map((d) => d.y));
+  };
 
   const handleClick = () => {
     setDayOrWeek((prev) => !prev);
@@ -63,6 +76,7 @@ const StatusChart = () => {
       <div className={styles.centering}>
         <VictoryChart
           theme={VictoryTheme.material}
+          // domain={{ y: [0, 1] }}
           containerComponent={
             <VictoryVoronoiContainer
               voronoiDimension="x"
@@ -72,6 +86,24 @@ const StatusChart = () => {
           }
           {...options}
         >
+          <VictoryAxis tickValues={dayOrWeek ? dates : weekly} />
+          <VictoryAxis
+            dependentAxis
+            orientation="left"
+            // tickValues={[0.25, 0.5, 0.75, 1]}
+            // tickFormat={(t) =>
+            // dayOrWeek ? t * Math.floor(getMax(dailyMainData)) : t * Math.floor(getMax(weeklyMainData))
+            // }
+          />
+          <VictoryAxis
+            dependentAxis
+            orientation="right"
+            // tickValues={[0.25, 0.5, 0.75, 1]}
+            // tickFormat={(t) =>
+            // dayOrWeek ? t * Math.floor(getMax(dailySubData)) : t * Math.floor(getMax(weeklySubData))
+            // }
+          />
+
           <VictoryLine
             name="main"
             // animate={{
@@ -82,7 +114,7 @@ const StatusChart = () => {
               data: { stroke: COLORS.YELLOW },
               parent: { border: '1px solid #ccc' },
             }}
-            data={dayOrWeek ? dailyData : weeklyData}
+            data={dayOrWeek ? dailyMainData : weeklyMainData}
           />
           {/* 옵셔널 그래프 */}
           <VictoryLine
@@ -95,10 +127,7 @@ const StatusChart = () => {
               data: { stroke: COLORS.ORANGE },
               parent: { border: '1px solid #ccc' },
             }}
-            data={[
-              { x: '2022-02-01', y: 6 },
-              { x: '2022-02-02', y: 4 },
-            ]}
+            data={dayOrWeek ? dailySubData : weeklySubData}
           />
         </VictoryChart>
       </div>
