cb3b7e035498cbdb935cf81e7cc09ac86772bacf
Min Jiwon <peace.cheejecake@gmail.com>
2022-05-25 10:56:23 +0900

    fix: Dynamic import for chart

--- src/components/AdStatus/index.tsx
+++ src/components/AdStatus/index.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
+import dynamic from 'next/dynamic';
 
 import DropButton from 'components/common/DropButton';
 import StatusCards from './StatusCards';
 import Term from './Term';
-import TestChart from './TestChart';
+// import TestChart from './TestChart';
 import styles from './style.module.scss';
 
+const TestChart = dynamic(() => import('./TestChart'), { ssr: false });
+
 const AdStatus = () => {
-  const [currentIdx, setCurrentIdx] = useState(0);
+  const [mainIdx, setMainIdx] = useState(0);
+  const [subIdx, setSubIdx] = useState(0);
   const items: DropItem[] = [
     {
       color: '#4FADF7',
@@ -26,8 +31,8 @@ const AdStatus = () => {
           <StatusCards />
         </ul>
         <div className={styles.selectWrapper}>
-          <DropButton dropItems={items} setCurrentIdx={setCurrentIdx} className={styles.dropButtonWrapper} />
-          <DropButton dropItems={items} setCurrentIdx={setCurrentIdx} className={styles.dropButtonWrapper} optional />
+          <DropButton dropItems={items} setCurrentIdx={setMainIdx} className={styles.dropButtonWrapper} />
+          <DropButton dropItems={items} setCurrentIdx={setSubIdx} className={styles.dropButtonWrapper} optional />
           <Term />
         </div>
         <div className={styles.chartWrapper}>
