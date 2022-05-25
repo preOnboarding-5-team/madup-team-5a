aa3ae59aec0be09fc97df095eb1d9a2a64b5ac19
Jaehyuk <jaehyukj3@gmail.com>
2022-05-25 13:35:28 +0900

    Term.tsx 버튼 에러 수정, 통합 광고 현황 디자인, 태그등  일부 수정

--- src/components/AdStatus/index.tsx
+++ src/components/AdStatus/index.tsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 
-import dynamic from 'next/dynamic';
-
 import DropButton from 'components/common/DropButton';
+import dynamic from 'next/dynamic';
 import StatusCards from './StatusCards';
 import Term from './Term';
 
 import styles from './style.module.scss';
-import dynamic from 'next/dynamic';
 
 const TestChart = dynamic(() => import('./TestChart'), { ssr: false });
-  
+
 const AdStatus = () => {
   const [mainIdx, setMainIdx] = useState(0);
   const [subIdx, setSubIdx] = useState(0);
@@ -26,17 +24,15 @@ const AdStatus = () => {
   ];
 
   return (
-    <section className={styles.adManageMainWrapper}>
-      <h1>통합 광고 현황</h1>
-      <div className={styles.adManageMain}>
+    <section className={styles.adStatusWrapper}>
+      <h2 className={styles.sectionTitle}>통합 광고 현황</h2>
+      <div className={styles.adStatus}>
         <ul className={styles.statusCardsWrapper}>
           <StatusCards />
         </ul>
         <div className={styles.selectWrapper}>
-          <div className={styles.dropButtons}>
-            <DropButton dropItems={items} setCurrentIdx={setMainIdx} className={styles.dropButton} />
-            <DropButton dropItems={items} setCurrentIdx={setSubIdx} className={styles.dropButton} optional />
-          </div>
+          <DropButton dropItems={items} setCurrentIdx={setMainIdx} className={styles.dropButton} />
+          <DropButton dropItems={items} setCurrentIdx={setSubIdx} className={styles.dropButton} optional />
           <Term />
         </div>
         <div className={styles.chartWrapper}>
